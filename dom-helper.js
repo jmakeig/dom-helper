948b64393131f2c6827bed3d36d36e60bbe7931a
Justin Makeig <jmakeig@marklogic.com>
2019-11-29 13:46:51 -0800

    Double quotes to single quotes

--- dom-helper.js
+++ dom-helper.js
@@ -7,8 +7,8 @@
  */
 function isIterable(item, ignoreStrings = true) {
 	if (!exists(item)) return false;
-	if ('function' === typeof item[Symbol.iterator]) {
-		return 'string' !== typeof item || !ignoreStrings;
+	if ("function" === typeof item[Symbol.iterator]) {
+		return "string" !== typeof item || !ignoreStrings;
 	}
 	return false;
 }
@@ -19,7 +19,7 @@ function isIterable(item, ignoreStrings = true) {
  * @return {boolean}
  */
 function exists(item) {
-	return !('undefined' === typeof item || null === item);
+	return !("undefined" === typeof item || null === item);
 }
 /**
  * Whether something doesn’t exist or is *not* an empty `string`
@@ -28,7 +28,7 @@ function exists(item) {
  * @return {boolean}
  */
 function isEmpty(item) {
-	return !exists(item) || '' === item;
+	return !exists(item) || "" === item;
 }
 /**
  * Guarantees an interable, even if passed a non-iterable,
@@ -58,8 +58,8 @@ function createElement(name) {
 function textNode(str) {
 	return document.createTextNode(
 		String(str)
-			.replace(/&/, '&amp;')
-			.replace(/</, '&lt;')
+			.replace(/&/, "&amp;")
+			.replace(/</, "&lt;")
 	);
 }
 
@@ -87,33 +87,33 @@ function applyToElement(param, el) {
 	}
 
 	switch (typeof param) {
-		case 'string':
-		case 'number':
-		case 'boolean':
+		case "string":
+		case "number":
+		case "boolean":
 			el.appendChild(textNode(String(param)));
 			return el;
-		case 'object':
+		case "object":
 			if (null === param) {
 				el.appendChild(textNode(String(param)));
 				return el;
 			}
 	}
 
-	if (exists(param) && 'object' === typeof param) {
+	if (exists(param) && "object" === typeof param) {
 		for (const p of [
 			...Object.getOwnPropertyNames(param),
 			...Object.getOwnPropertySymbols(param)
 		]) {
 			switch (p) {
-				case 'style':
-				case 'dataset':
+				case "style":
+				case "dataset":
 					for (let item in param[p]) {
 						if (exists(item)) el[p][item] = param[p][item];
 					}
 					break;
-				case 'class':
-				case 'className':
-				case 'classList':
+				case "class":
+				case "className":
+				case "classList":
 					for (const cls of toIterable(param[p])) {
 						if (exists(cls)) el.classList.add(cls);
 					}
@@ -148,51 +148,51 @@ export const toFragment = (...rest) => element(null, ...rest);
 export const empty = () => toFragment();
 export const text = textNode;
 
-export const header = (...rest) => element('header', ...rest);
-export const nav = (...rest) => element('nav', ...rest);
-export const footer = (...rest) => element('footer', ...rest);
-export const div = (...rest) => element('div', ...rest);
-export const p = (...rest) => element('p', ...rest);
-export const h1 = (...rest) => element('h1', ...rest);
-export const h2 = (...rest) => element('h2', ...rest);
-export const h3 = (...rest) => element('h3', ...rest);
-export const h4 = (...rest) => element('h4', ...rest);
-export const h5 = (...rest) => element('h5', ...rest);
-export const h6 = (...rest) => element('h6', ...rest);
+export const header = (...rest) => element("header", ...rest);
+export const nav = (...rest) => element("nav", ...rest);
+export const footer = (...rest) => element("footer", ...rest);
+export const div = (...rest) => element("div", ...rest);
+export const p = (...rest) => element("p", ...rest);
+export const h1 = (...rest) => element("h1", ...rest);
+export const h2 = (...rest) => element("h2", ...rest);
+export const h3 = (...rest) => element("h3", ...rest);
+export const h4 = (...rest) => element("h4", ...rest);
+export const h5 = (...rest) => element("h5", ...rest);
+export const h6 = (...rest) => element("h6", ...rest);
 
-export const ul = (...rest) => element('ul', ...rest);
-export const ol = (...rest) => element('ol', ...rest);
-export const li = (...rest) => element('li', ...rest);
-export const dl = (...rest) => element('dl', ...rest);
-export const dt = (...rest) => element('dt', ...rest);
-export const dd = (...rest) => element('dd', ...rest);
+export const ul = (...rest) => element("ul", ...rest);
+export const ol = (...rest) => element("ol", ...rest);
+export const li = (...rest) => element("li", ...rest);
+export const dl = (...rest) => element("dl", ...rest);
+export const dt = (...rest) => element("dt", ...rest);
+export const dd = (...rest) => element("dd", ...rest);
 
-export const table = (...rest) => element('table', ...rest);
-export const thead = (...rest) => element('thead', ...rest);
-export const tfoot = (...rest) => element('tfoot', ...rest);
-export const tbody = (...rest) => element('tbody', ...rest);
-export const tr = (...rest) => element('tr', ...rest);
-export const th = (...rest) => element('th', ...rest);
-export const td = (...rest) => element('td', ...rest);
+export const table = (...rest) => element("table", ...rest);
+export const thead = (...rest) => element("thead", ...rest);
+export const tfoot = (...rest) => element("tfoot", ...rest);
+export const tbody = (...rest) => element("tbody", ...rest);
+export const tr = (...rest) => element("tr", ...rest);
+export const th = (...rest) => element("th", ...rest);
+export const td = (...rest) => element("td", ...rest);
 
-export const span = (...rest) => element('span', ...rest);
-export const a = (...rest) => element('a', ...rest);
-export const em = (...rest) => element('em', ...rest);
-export const strong = (...rest) => element('strong', ...rest);
-export const mark = (...rest) => element('mark', ...rest);
+export const span = (...rest) => element("span", ...rest);
+export const a = (...rest) => element("a", ...rest);
+export const em = (...rest) => element("em", ...rest);
+export const strong = (...rest) => element("strong", ...rest);
+export const mark = (...rest) => element("mark", ...rest);
 
-export const input = (...rest) => element('input', { type: 'text' }, ...rest);
-export const button = (...rest) => element('button', ...rest);
-export const textarea = (...rest) => element('textarea', ...rest);
+export const input = (...rest) => element("input", { type: "text" }, ...rest);
+export const button = (...rest) => element("button", ...rest);
+export const textarea = (...rest) => element("textarea", ...rest);
 export const checkbox = (...rest) =>
-	element('input', { type: 'checkbox' }, ...rest);
-export const radio = (...rest) => element('input', { type: 'radio' }, ...rest);
-export const select = (...rest) => element('select', ...rest);
-export const option = (...rest) => element('option', ...rest);
-export const file = (...rest) => element('input', { type: 'file' }, ...rest);
+	element("input", { type: "checkbox" }, ...rest);
+export const radio = (...rest) => element("input", { type: "radio" }, ...rest);
+export const select = (...rest) => element("select", ...rest);
+export const option = (...rest) => element("option", ...rest);
+export const file = (...rest) => element("input", { type: "file" }, ...rest);
 
-export const br = (...rest) => element('br', ...rest);
-export const hr = (...rest) => element('hr', ...rest);
+export const br = (...rest) => element("br", ...rest);
+export const hr = (...rest) => element("hr", ...rest);
 
 /**
  * Replaces the entire contents of `oldNode` with `newChild`.
